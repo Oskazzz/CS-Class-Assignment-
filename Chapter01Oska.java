3540654f07bdf35616dbb3a2d09678986cf2882f
Oskazzz <31762162+Oskazzz@users.noreply.github.com>
2017-09-12 12:20:52 +0800

    Add files via upload

---
+++ Chapter01Oska.java
@@ -0,0 +1,11 @@
+
+public class Chapter01Oska { 
+	public static void main(String[] args ) {
+		//generate some simple output 
+		System.out.println("1.4\tClass, method, statement and comment\nHello,World!");
+		System.out.println("1.5\"print\" versus\"println\"\nHello,World!\nThis example also shows the escape sequence\"\\\"\"  ");
+		System.out.println("1.6.1 The escape sequence \"\\n\"\n(1)Hello,\nWorld!\n(2) Hello,/nWorld!");
+		System.out.println("1.6.2 The escape sequence\"\\t\"\nHello,World!\nHello,\n\tWorld!\n(1)Hello,\tWorld!\n(2)Hello,\t\n\tWorld!\n\n(5) He said: \"Hello, World!\"\n(6)Hello,World!\\\\");
+	}
+
+}
